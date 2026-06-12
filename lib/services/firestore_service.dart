49d32e9e354f9349773820097924bea1325ac035
redmediasolutions <redmediasolutions8@gmail.com>
2026-06-12 11:57:47 +0530

    Update firestore_service.dart

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -478,6 +478,7 @@ Future<void> approveRewardWithdrawal(
             FieldValue.serverTimestamp(),
       },
     );
+    //TEST
 
     /// Find matching wallet transaction
     final walletTxQuery =
